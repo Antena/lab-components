9ce1623d4347e5e4db5a7e68035b781200b316ab
Denise Fernandez <denise@antena.co>
2016-05-19 15:39:33 -0300

    better support for top-level observations

--- src/lab-diagnostic-report/lab-diagnostic-report-directive.js
+++ src/lab-diagnostic-report/lab-diagnostic-report-directive.js
@@ -26,16 +26,31 @@ module.exports = function($rootScope, $document, $filter) {
 
 			$scope.$watch('vm.observations', function(observations) {
 				$scope.vm.groupedObservations = _.groupBy(observations, function(obs) {
-					return obs.extension[0].valueIdentifier.value;
+					return obs.extension && obs.extension[0].valueIdentifier ? obs.extension[0].valueIdentifier.value : obs.id;
 				});
 			});
 
 			$scope.getOrderItemDisplay = function(itemCode) {
+				var result = "";
+
 				var item = _.find($scope.vm.diagnosticOrder.item, function(item) {
 					return item.code.extension[0].valueIdentifier.value === itemCode;
 				});
 
-				return item.code.coding[0].display;
+				if(item) {
+					result = item.code.coding[0].display;
+				} else {
+					var observation = _.findWhere($scope.vm.observations, {id: itemCode});
+					if(observation) {
+						result = observation.code.coding[0].display;
+					}
+				}
+
+				return result;
+			};
+
+			$scope.isTopLevel = function(group, observationsPerGroup) {
+				return observationsPerGroup.length === 1 && observationsPerGroup[0].id === group;
 			};
 
 			//TODO (denise) check if still used here
