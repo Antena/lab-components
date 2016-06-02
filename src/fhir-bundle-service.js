fe6fcd62b1585447be2f6192428143478e3eb6e7
Denise Fernandez <denise@antena.co>
2016-06-02 15:53:45 -0300

    retrieve concrete order if identifier is provider

--- src/fhir-bundle-service.js
+++ src/fhir-bundle-service.js
@@ -10,6 +10,20 @@ module.exports = function() {
 		return lastSlash > -1 ? reference.substring(lastSlash + 1, reference.length) : null;
 	}
 
+	function resolveFromOrder(order, fhirBundleResources) {
+		order.orderer = lodash.cloneDeep(_.findWhere(fhirBundleResources, {
+			resourceType: "Practitioner",
+			id: getReferencedId(order.orderer.reference)
+		}));
+
+		order.subject = lodash.cloneDeep(_.findWhere(fhirBundleResources, {
+			resourceType: "Patient",
+			id: getReferencedId(order.subject.reference)
+		}));
+
+		return order;
+	}
+
 	function resolveOrder(fhirBundleResources, orderReference) {
 		var searchParams = {resourceType: "DiagnosticOrder"};
 		if (orderReference) {
@@ -18,29 +32,35 @@ module.exports = function() {
 
 		var order = lodash.cloneDeep(_.findWhere(fhirBundleResources, searchParams));
 
-		order.orderer =  lodash.cloneDeep(_.findWhere(fhirBundleResources, {
-			resourceType: "Practitioner",
-			id: getReferencedId(order.orderer.reference)
-		}));
-
-		order.subject =  lodash.cloneDeep(_.findWhere(fhirBundleResources, {
-			resourceType: "Patient",
-			id: getReferencedId(order.subject.reference)
-		}));
-
-		return order;
+		return resolveFromOrder(order, fhirBundleResources);
 	}
 
 	return {
-		resolveOrderAndReportReferences: function(fhirBundle) {
+		resolveOrderAndReportReferences: function(fhirBundle, orderValueIdentifier) {
 
 			var fhirBundleResources = _.pluck(fhirBundle.entry, 'resource');
 
 			/* Order */
-			var order = resolveOrder(fhirBundleResources);
+			var order;
+			if(!orderValueIdentifier) {
+				// use the first one if no orderValueIdentifier provided
+				order = resolveOrder(fhirBundleResources);
+			} else {
+				var orders = _.where(fhirBundleResources, { resourceType: "DiagnosticOrder" });
+				var rawOrder =_.find(orders, function(o) {
+					return o.identifier[0].value === orderValueIdentifier;
+				});
+				order = resolveFromOrder(rawOrder, fhirBundleResources);
+			}
 
+			var reports = _.where(fhirBundleResources, { resourceType: "DiagnosticReport"});
+			var orderId = "DiagnosticOrder/" + order.id;
+			
 			/* Report */
-			var report = lodash.cloneDeep(_.findWhere(fhirBundleResources, { resourceType: "DiagnosticReport"}));
+			var rawReport =_.find(reports, function(r) {
+				return r.requestDetail[0].reference === orderId;
+			});
+			var report = lodash.cloneDeep(rawReport);
 			report.subject = _.findWhere(fhirBundleResources, { resourceType: "Patient", id: getReferencedId(report.subject.reference)});
 			report.performer = _.findWhere(fhirBundleResources, { resourceType: "Organization", id: getReferencedId(report.performer.reference)});
 			report.requestDetail = _.map(report.requestDetail, function(requestDetail) {
