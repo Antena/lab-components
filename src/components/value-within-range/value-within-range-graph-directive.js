de0def550da21b13f39efc9809956747a8c19d95
Gonzalo Bellver <gonzalo@antena.co>
2016-06-10 20:16:52 -0300

    Fixed valueWithinRangeGraph rendering issues CV-1479

--- src/components/value-within-range/value-within-range-graph-directive.js
+++ src/components/value-within-range/value-within-range-graph-directive.js
@@ -11,7 +11,7 @@
  * display the domain and range with different visual aids, and will highlight the value differently, depending
  * on whether it's within the given range, or outside of it.
  *
- * The domain is optional, and will be defined as a percentage outside of the range if not provided.
+ * The domain is optional, and will be defined as a percentage of the range's amplitude if not provided.
  * The range and value must always belong to the given domain.
  *
  * Additionally, a unit for the value can be provided, which will be displayed next to the value.
@@ -24,7 +24,7 @@
  * @param {Object} range An object representing the range to be displayed.
  * This must contain two numeric properties: 'low' and 'high'.
  *
- * @param {Object=} [domain={ low: range.low * 0.1, high: range.high * 0.1}] An object representing the range to be displayed. This must contain two numeric properties: 'low' and 'high'.
+ * @param {Object=} domain An object representing the range to be displayed. This cab contain two numeric properties: 'low' and 'high', which are the low and high absolute values of the domain.
  *
  * @param {String=} insideClass A class name to be applied to the value element, when it falls inside the range.
  * If not provided, 'rangeClass' will be used.
@@ -70,6 +70,18 @@
 		 		  	ng-model="example.range.high" required>
 			    </div>
 		 	  </div>
+  			<div class="col col-third">
+ 				<div class="form-group">
+  				  <label for="domainLow" class="control-label">domain.low</label>
+ 				  <input type="number" class="form-control input-sm" id="domainLow"
+ 					ng-model="example.domain.low" required>
+ 				</div>
+ 				<div class="form-group">
+  				  <label for="domainHigh" class="control-label">domain.high</label>
+  				  <input type="number" class="form-control input-sm" id="domainHigh"
+ 					ng-model="example.domain.high" required>
+ 				</div>
+ 			</div>
 			</div>
 		  </form>
 
@@ -125,25 +137,21 @@ var angular = require('angular');
 // @ngInject
 module.exports = function() {
 
-	function calculateDomainFromRange(value, range, bleedFactor) {
-		var domainLow = function(value, range) {
-			if (value < range.low) {
-				return value - value * bleedFactor;
-			} else {
-				return range.low - range.low * bleedFactor;
-			}
-		};
-		var domainHigh = function(value, range) {
-			if (value > range.high) {
-				return value + value * bleedFactor;
-			} else {
-				return range.high + range.high * bleedFactor;
-			}
-		};
-
+	/**
+	 * Calculates the domain of the scale based on a value and range.
+	 * May apply a bleed factor based on the range, and can be overrided.
+	 *
+	 * @param domain: the domain override.
+	 * @param value: the value.
+	 * @param range: the low and high values of the range.
+	 * @param bleedFactor: the desired bleed factor.
+	 * @returns {{low: number, high: number}}
+     */
+	function calculateDomain(domain, value, range, bleedFactor) {
+		var f = (range.high - range.low) * bleedFactor;
 		return {
-			low: domainLow(value, range),
-			high: domainHigh(value, range)
+			low: domain && !isNaN(domain.low) ? domain.low : (Math.min(value, range.low) - f),
+			high: domain && !isNaN(domain.high) ? domain.high : (Math.max(value, range.high) + f)
 		};
 	}
 
@@ -167,12 +175,12 @@ module.exports = function() {
 				}, 300),
 				validModel = function() {
 					return _.isNumber(scope.value) &&
-							_.isObject(scope.range) &&
-							_.has(scope.range, 'low') &&
-							_.has(scope.range, 'high') &&
-							_.isNumber(scope.range.low) &&
-							_.isNumber(scope.range.high) &&
-							scope.range.high > scope.range.low;
+						_.isObject(scope.range) &&
+						_.has(scope.range, 'low') &&
+						_.has(scope.range, 'high') &&
+						_.isNumber(scope.range.low) &&
+						_.isNumber(scope.range.high) &&
+						scope.range.high > scope.range.low;
 				};
 
 			function watchModel(modelName) {
@@ -183,17 +191,17 @@ module.exports = function() {
 				});
 			}
 
-
 			//TODO (denise) this sucks
 			watchModel('value');
 			watchModel('unit');
 			watchModel('range.low');
 			watchModel('range.high');
+			watchModel('domain.low');
+			watchModel('domain.high');
 
-
-			// var parent = $(elem[0].parentNode);
-
-			var width = 256,
+			var svg,
+				scale,
+				width = 0,
 				height = 50,
 				circleRadius = 5,
 				strokeWeight = 2,
@@ -205,98 +213,126 @@ module.exports = function() {
 					right: 20
 				};
 
-			var svg = d3.select(elem[0]).append('svg')
-				.attr('width', width)
-				.attr('height', height)
-				.classed('value-within-range-graph', true);
+			// Setting timeout 0 to ensure Angular's digest cycle has finished, because... Angular.
+			setTimeout(function() {
+				var container = elem.parent()[0];
+				width = angular.element(container).width();
+				init();
+			}, 0);
 
-			refresh();
+
+			/**
+			 * Draw the basic elements with its constant properties
+			 */
+			function init() {
+				// SVG
+				svg = d3.select(elem[0]).append('svg')
+					.attr('width', width)
+					.attr('height', height)
+					.classed('value-within-range-graph', true);
+
+				// X-Scale
+				scale = d3.scale.linear()
+					.range([padding.left, width - padding.right]);
+
+				// Low range line + text
+				svg.append('line')
+					.classed('low-range-line', true)
+					.attr("stroke-width", strokeWeight);
+				svg.append('text')
+					.classed('low-range-text', true)
+					.attr('dy', '0.375em');
+
+				// Healthy range line
+				svg.append('line')
+					.attr("stroke-width", strokeWeight)
+					.classed('inside-range-line', true);
+
+				// High range line + text
+				svg.append('line')
+					.classed('high-range-line', true)
+					.attr("stroke-width", strokeWeight);
+				svg.append('text')
+					.classed('high-range-text', true)
+					.attr('dy', '0.375em');
+
+				// Value circle + text
+				svg.append('circle')
+					.classed('value-point', true)
+					.attr("cy", yPos)
+					.attr("r", circleRadius);
+				svg.append('text')
+					.classed('value-text', true)
+					.attr('y', yPos - yOffset)
+					.attr('dy', 0);
+
+				refresh();
+			}
 
 
+			/**
+			 * Re-draw the graph when something changes
+			 */
 			function refresh() {
-
-				var svgElem = $(elem[0]).find('svg');
-				svgElem.empty();
-
+				// Data
 				var value = parseFloat(scope.value),
 					unit = scope.unit,
 					range = scope.range,
 					insideClass = scope.insideClass || scope.rangeClass,
 					outsideClass = scope.insideClass || scope.domainClass;
 
-				var domain = scope.domain || calculateDomainFromRange(value, range, bleedFactor);
+				// Update: scale's domain
+				var domain = calculateDomain(scope.domain, value, range, bleedFactor);
+				scale.domain([domain.low, domain.high]);
 
-				var scale = d3.scale.linear()
-					.domain([domain.low, domain.high])
-					.range([padding.left, width - padding.right]);
-
-				// Low range
-				svg.append('line')
+				// Update: low range line + text
+				svg.select('line.low-range-line')
 					.attr('x1', padding.left)
 					.attr('y1', yPos)
 					.attr('x2', scale(range.low))
 					.attr('y2', yPos)
-					.attr("stroke-width", strokeWeight)
-					.classed('low-range-line', true)
 					.classed(scope.domainClass, true);
-				svg.append('text')
+				svg.select('text.low-range-text')
 					.attr('x', scale(range.low))
 					.attr('y', yPos + yOffset)
-					.attr('dy', '0.375em')
 					.attr('text-anchor', 'middle')
-					.classed('low-range-text', true)
 					.text(range.low);
 
-				// Healthy range
-				svg.append('line')
+				// Update: healthy range
+				svg.select('line.inside-range-line')
 					.attr('x1', scale(range.low))
 					.attr('y1', yPos)
 					.attr('x2', scale(range.high))
 					.attr('y2', yPos)
-					.attr("stroke-width", strokeWeight)
-					.classed(scope.rangeClass, true)
-					.classed('inside-range-line', true);
+					.classed(scope.rangeClass, true);
 
-				// High range
-				svg.append('line')
+				// Update: high range
+				svg.select('line.high-range-line')
 					.attr('x1', scale(range.high))
 					.attr('y1', yPos)
 					.attr('x2', width - padding.right)
 					.attr('y2', yPos)
-					.attr("stroke-width", strokeWeight)
-					.classed('high-range-line', true)
 					.classed(scope.domainClass, true);
-				svg.append('text')
+				svg.select('text.high-range-text')
 					.attr('x', scale(range.high))
 					.attr('y', yPos + yOffset)
-					.attr('dy', '0.375em')
 					.attr('text-anchor', 'middle')
-					.classed('high-range-text', true)
 					.text(range.high);
 
-				// Value
-				var valueWithinRange = value >= range.low && value <= range.high;
-				var valueInDomain = value < range.low || value > range.high;
-				var valueText = value + (unit ? (' ' + unit) : '');
-
-				svg.append('text')
+				// Update: value
+				var valueWithinRange = value >= range.low && value <= range.high,
+					valueInDomain = value < range.low || value > range.high,
+					textAnchor = value < range.low ? 'start' : ( value > range.high ? 'end': 'middle');
+				svg.select('text.value-text')
 					.attr('x', scale(value))
-					.attr('y', yPos - yOffset)
-					.attr('dy', '0.375em')
-					.attr('dy', 0)
-					.attr('text-anchor', 'middle')
+					.attr('text-anchor', textAnchor)
 					.classed(outsideClass, valueInDomain && !valueWithinRange)
 					.classed(insideClass, valueWithinRange)
-					.classed('value-text', true)
-					.text(valueText);
-
-				svg.append('circle')
+					.text(value + (unit ? (' ' + unit) : ''));
+				svg.select('circle.value-point')
 					.attr("cx", scale(value))
-					.attr("cy", yPos)
-					.attr("r", circleRadius)
 					.classed(outsideClass, valueInDomain && !valueWithinRange)
-					.classed(insideClass, valueWithinRange)
-					.classed('value-point', true);
+					.classed(insideClass, valueWithinRange);
 			}
 		}
 	};
