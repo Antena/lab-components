b2e7808abc089c4f07252cec96f03c1e3b4b70e8
Gonzalo Bellver <gonzalo@antena.co>
2016-07-11 16:33:49 -0300

    Basic documentation CV-1574

--- src/components/lab-history/sparkline-directive.js
+++ src/components/lab-history/sparkline-directive.js
@@ -1,3 +1,73 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name lab-components.components.sparkline.directive:sparkline
+ * @restrict AE
+ * @scope
+ *
+ * @description
+ * //TODO(gb): add description
+ *
+ * @element ANY
+ * @param {Array} values An array of objects of form { date: String, value: Number } where date is a formatted string date.
+ * @param {Object=} [options] An object iwth options to override:
+ * Defaults:
+ * ```js
+ * {
+ *		width: 100,
+ *		height: 25,
+ *		padding: { left: 5, right: 5, bottom: 5, top: 5 },
+ *		circleRadius: 2,
+ *		defaultDateHistory: 1000 * 60 * 60 * 24 * 30 * 6,
+ *		dateFormat: '%Y-%m-%d'
+ * }
+ * ```
+ *
+ * @example
+ <example module="sparkline-example">
+ 	<file name="index.html">
+ 		<div ng-controller="ExampleController" class="example">
+			 <sparkline
+ 				values="example.values"
+ 				options="example.options">
+			 </sparkline>
+ 		</div>
+ 	</file>
+
+ 	<file name="demo.js">
+ 		angular.module('sparkline-example', ['lab-components.components.sparkline'])
+ 			.controller('ExampleController', ['$scope', function($scope) {
+				$scope.example = {
+					values: [
+						{ date: "2016-06-29", value: 55 },
+						{ date: "2016-06-21", value: 60 },
+						{ date: "2016-06-02", value: 63 },
+						{ date: "2016-05-03", value: 52 },
+						{ date: "2016-04-03", value: 51 },
+						{ date: "2016-03-03", value: 50 },
+						{ date: "2016-01-10", value: 48 },
+						{ date: "2015-01-10", value: 55 },
+						{ date: "2014-01-10", value: 58 }
+					],
+					options: { }
+				};
+
+				$scope.example.json = angular.toJson($scope.example.values);
+
+				$scope.$watch('example.json', function(newValue) {
+					if (newValue) {
+						$scope.example.observation = angular.fromJson(newValue);
+					}
+				});
+			}]
+ 		);
+
+ 	</file>
+ </example>
+ */
+
+
 var d3 = require('d3');
 var _ = require('underscore');
 var angular = require('angular');
@@ -19,7 +89,8 @@ module.exports = function() {
 				height: 25,
 				padding: { left: 5, right: 5, bottom: 5, top: 5 },
 				circleRadius: 2,
-				defaultDateHistory: 1000 * 60 * 60 * 24 * 30 * 6	// 6 months
+				defaultDateHistory: 1000 * 60 * 60 * 24 * 30 * 6,	// 6 months
+				dateFormat: '%Y-%m-%d'
 			});
 
 			var width = options.width - options.padding.left - options.padding.right,
@@ -33,7 +104,7 @@ module.exports = function() {
 			var x = d3.scale.linear().range([options.padding.left, options.width - options.padding.right]);
 			var y = d3.scale.linear().range([options.height - options.padding.bottom, options.padding.top]);
 
-			var parseDate = d3.time.format('%Y-%m-%d').parse;
+			var parseDate = d3.time.format(options.dateFormat).parse;
 
 			var line = d3.svg.line()
 				.x(function(d) { return x(d.date); })
