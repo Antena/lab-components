6ebb7415856a6c2cc3d5b91f6ec136ed17b88857
Denise Fernandez <denise@antena.co>
2016-07-12 14:20:50 -0300

    better calculation of incorporated ranges borders

--- src/lab-observation/lab-observation-multirange/lab-observation-multirange-controller.js
+++ src/lab-observation/lab-observation-multirange/lab-observation-multirange-controller.js
@@ -99,51 +99,91 @@ module.exports = function($scope, $filter) {
 		return observation.referenceRange.length === 1 && observation.referenceRange[0].low && observation.referenceRange[0].high;
 	}
 
+	function getDecimalCount(value) {
+		return Math.floor(value) === value ? 0 : (value.toString().split(".")[1].length || 0);
+	}
+
+	function calculateLowBorder(originalRange, observation) {
+		var highDecimals = getDecimalCount(originalRange.high.value);
+		var lowDecimals = getDecimalCount(originalRange.low.value);
+		var valueDecimals = getDecimalCount(observation.valueQuantity.value);
+		var decimalCount = Math.max(valueDecimals, Math.max(highDecimals, lowDecimals));
+		var minUnit;
+
+		var diffToApply = 0;
+
+		if (originalRange.low.value > 0) {
+			if (decimalCount > 0) {
+				minUnit = Math.pow(10, decimalCount);
+				diffToApply = 1 / minUnit;
+			} else {
+				// var digits = ("" + originalRange.low.value).length;
+				// minUnit = Math.pow(10, digits);
+				// diffToApply = 1 * minUnit;
+
+				diffToApply = 1;
+			}
+		}
+
+		var lowBorder = originalRange.low.value - diffToApply;
+		if (decimalCount > 0) {
+			lowBorder = parseFloat(lowBorder.toFixed(decimalCount));
+		}
+		return lowBorder;
+	}
+
 	function fillMissingRanges(observation) {
 		var originalRange = observation.referenceRange[0];
 
+		var lowBorder = calculateLowBorder(originalRange, observation);
+
 		var result = [
+			{
+				low: {
+					value: $scope.vm.graphOptions.domain.low,
+					units: originalRange.low.units,
+					system: originalRange.low.system,
+					code: originalRange.low.code
+				},
+				high: {
+					value: lowBorder,
+					units: originalRange.low.units,
+					system: originalRange.low.system,
+					code: originalRange.low.code
+				},
+				meaning: {
+					coding: [
 						{
-							high: originalRange.low,
-							low: {
-								value: $scope.vm.graphOptions.domain.low,
-								units: originalRange.low.units,
-								system: originalRange.low.system,
-								code: originalRange.low.code
-							},
-							meaning: {
-								coding: [
-									{
-										system: "http://hl7.org/fhir/v2/0078",
-										code: "L"
-									}
-								]
-							}
-						},
-						{
-							high: originalRange.high,
-							low: originalRange.low,
-							meaning: {
-								coding: [
-									{
-										system: "http://hl7.org/fhir/v2/0078",
-										code: "N" 	//TODO (denise) remove onces this is fixed from KERN!!!!!
-									}
-								]
-							}
-						},
-						{
-							low: originalRange.high,
-							meaning: {
-								coding: [
-									{
-										system: "http://hl7.org/fhir/v2/0078",
-										code: "H"
-									}
-								]
-							}
+							system: "http://hl7.org/fhir/v2/0078",
+							code: "L"
 						}
-						];
+					]
+				}
+			},
+			{
+				low: originalRange.low,
+				high: originalRange.high,
+				meaning: {
+					coding: [
+						{
+							system: "http://hl7.org/fhir/v2/0078",
+							code: "N" 	//TODO (denise) remove onces this is fixed from KERN!!!!!
+						}
+					]
+				}
+			},
+			{
+				low: originalRange.high,
+				meaning: {
+					coding: [
+						{
+							system: "http://hl7.org/fhir/v2/0078",
+							code: "H"
+						}
+					]
+				}
+			}
+		];
 
 		if (originalRange.low.value === $scope.vm.graphOptions.domain.low) {
 			result.shift();
