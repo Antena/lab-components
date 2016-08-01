a488b066737f378f9150c3ff0f5b7c7c1255a887
Denise Fernandez <denise@antena.co>
2016-08-01 11:42:57 -0300

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,7 @@ var config = {
 			jquery: 'jquery/dist/jquery.min.js',
 			moment: 'moment/moment.js',
 			underscore: 'underscore/underscore-min.js',
-			uiRouter: 'angular-ui-router/release/angular-ui-router.min.js',
-			snapjs: 'snap.js/dist/snap.min.js'
+			uiRouter: 'angular-ui-router/release/angular-ui-router.min.js'
 		}
 	},
 	plugins: [
