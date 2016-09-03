7158b1c986a5e41bb616d3ee7fd21fcb1a30d961
Denise Fernandez <denise@antena.co>
2016-09-02 21:12:06 -0300

    headerActions support featurePreview configured from component client

--- src/lab-observation/lab-observation-directive.js
+++ src/lab-observation/lab-observation-directive.js
@@ -28,33 +28,33 @@
 
  <div ng-controller="ExampleController" class="example">
 
-	<label>Observation Json:</label>
- 	<textarea class="form-control" rows="5" ng-model="example.json"></textarea>
+ <label>Observation Json:</label>
+ <textarea class="form-control" rows="5" ng-model="example.json"></textarea>
 
-	 <lab-observation
-		observation="example.observation"
-		actions="example.actions"
-		header-actions="example.headerActions">
-		<p ng-if="example.observation.showContent" class="inner-content">Transcluded content goes here. Observation id: {{ example.observation.id }}</p>
-	 </lab-observation>
+ <lab-observation
+ observation="example.observation"
+ actions="example.actions"
+ header-actions="example.headerActions">
+ <p ng-if="example.observation.showContent" class="inner-content">Transcluded content goes here. Observation id: {{ example.observation.id }}</p>
+ </lab-observation>
  </div>
 
  </file>
  <file name="styles.css">
 
-	.example .unhealthy {
+ .example .unhealthy {
 		color: #C0334E;
 	}
 
-	.example .healthy {
+ .example .healthy {
 		color: #00b752;
 	}
 
-	.example textarea {
+ .example textarea {
 		width: 98%;
 	}
 
- 	.example .inner-content {
+ .example .inner-content {
  		background-color: #FFFF66;
  	}
 
@@ -62,7 +62,7 @@
  <file name="demo.js">
 
  angular.module('lab-observation-example', ['lab-components.lab-observation'])
- 	.controller('ExampleController', ['$scope', function($scope) {
+ .controller('ExampleController', ['$scope', function($scope) {
 			$scope.example = {
 				json: "",
 				observation: { "resourceType": "Observation", "id": "5", "meta": { "versionId": "1", "lastUpdated": "2016-05-10T15:58:57.000+00:00" }, "extension": [ { "url": "http://www.cdrossi.com/relacion_prestacion_resultado_prestacion", "valueIdentifier": { "system": "http://www.cdrossi.com/pedido_prestacion", "value": "810-2547-4" } } ], "text": { "status": "empty", "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"/>" }, "code": { "coding": [ { "display": "T4 - TIROXINA TOTAL" } ] }, "valueQuantity": { "value": 7.8, "units": "µg/dl", "system": "http://unitsofmeasure.org", "code": "mg/dL" }, "comments": "-", "status": "final", "method": { "text": "Electroquimioluminiscencia" }, "identifier": [ { "system": "http://www.cdrossi.com.ar/resultados", "value": "810-2547-P.866-1" } ], "subject": { "reference": "Patient/1" }, "performer": [ { "reference": "Organization/3" } ], "interpretation": { "coding": [ { "system": "http://hl7.org/fhir/v2/0078", "code": "N" } ] }, "referenceRange": [ { "modifierExtension": [ { "url": "http://www.cdrossi.com.ar/validezrangoreferencia", "valuePeriod": { "start": "2013-01-01T00:00:00" } } ], "low": { "value": 4.6, "units": "µg/dl", "system": "http://unitsofmeasure.org", "code": "µg/dl" }, "high": { "value": 12, "units": "µg/dl", "system": "http://unitsofmeasure.org", "code": "µg/dl" }, "meaning": { "coding": [ { "system": "http://hl7.org/fhir/v2/0078", "code": "N" } ] } } ], "showHistory": 0 },
@@ -111,7 +111,7 @@
 				}
 			});
 		}])
- 	.config(['$translateProvider', function($translateProvider) {
+ .config(['$translateProvider', function($translateProvider) {
 			$translateProvider
 				.translations('es', {
 					  "LAB": {
@@ -183,6 +183,11 @@ module.exports = function() {
 
 			$scope.onActionLeave = function() {
 				$scope.hovered = null;
+			};
+
+			$scope.onActionClick = function(action, observation, $event) {
+				action.click(observation, action);
+				$scope.onActionLeave();
 			};
 
 			$scope.$watch('observation', function(observation) {
