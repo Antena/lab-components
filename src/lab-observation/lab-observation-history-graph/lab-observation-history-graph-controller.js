11ba7b520adb48c8274e5d963ceccfdfbc2166d6
Denise Fernandez <denise@antena.co>
2017-01-11 16:13:59 -0300

    fixes no refRange view of history, sparkline width, precision for graph points

--- src/lab-observation/lab-observation-history-graph/lab-observation-history-graph-controller.js
+++ src/lab-observation/lab-observation-history-graph/lab-observation-history-graph-controller.js
@@ -3,7 +3,17 @@
 var _ = require('underscore');
 
 // @ngInject
-module.exports = function ($scope, FhirReferenceRangeConverterService) {
+module.exports = function ($scope, $filter, FhirReferenceRangeConverterService) {
+	function obtainNumericValue(obs) {
+		var numericValue = obs.valueQuantity.value;
+		var precisionExtension = _.findWhere(obs.extension, {url: "http://www.cdrossi.com/precision"});
+		if (precisionExtension) {
+			var precision = precisionExtension.valueInteger;
+			numericValue = $filter('number')(obs.valueQuantity.value, precision);
+		}
+		return numericValue;
+	}
+
 	function processReferenceRange(observation) {
 		var ranges = [];
 
@@ -22,10 +32,17 @@ module.exports = function ($scope, FhirReferenceRangeConverterService) {
 
 	// Data points
 	var data = [];
-	_.each(_.filter($scope.observationList, function(obs) { return obs.resourceType == 'Observation' && !_.isEmpty(obs.valueQuantity) && !_.isEmpty(obs.issued) }), function(obs) {
+	var filtered = _.filter($scope.observationList, function(obs) {
+		return obs.resourceType == 'Observation' && !_.isEmpty(obs.valueQuantity) && !_.isEmpty(obs.issued);
+	});
+
+	_.each(filtered, function(obs) {
 		var datum = {};
 		datum.date = obs.issued;
-		datum.value = obs.valueQuantity.value;
+
+		var numericValue = obtainNumericValue(obs);
+
+		datum.value = numericValue;
 		datum.ranges = processReferenceRange(obs);
 
 		data.push(datum);
